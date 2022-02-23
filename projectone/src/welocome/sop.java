ce9596e0b634356eb6af7c87db12093f18c95aec
Surendra Babu M <Surendra Babu M@192.168.0.103>
2022-02-23 20:03:43 +0530

    project one

---
+++ projectone/src/welocome/sop.java
@@ -0,0 +1,13 @@
+package welocome;
+
+public class sop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Surendra");
+		System.out.print("Mupparaju Surendra");
+
+	}
+
+}
